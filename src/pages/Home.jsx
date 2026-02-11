93fb05c11c85f039c85f409ff240d176196c060d
Mjacm15 <alfamar2005@gmail.com>
2026-02-11 17:46:16 -0500

    feat(TopMFilms):call api to get top 5 films

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,54 @@
+import { useState, useEffect } from 'react';
+import api from '../axios.jsx';
+
+
+
 function Home() {
-  return <h2>Home Page</h2>;
+  const [films, setFilms]=useState([]);
+
+  // when component(whats in return) loads
+  useEffect(()=>{
+
+    const fetchedFilms= async ()=>{
+      try{
+        //api from axios
+        //with axios no need to convert from json(done automatically)
+        const response =await api.get('/films/top');//gets from api
+        setFilms(response.data);//re-renders/changes the state/sets films    
+
+      }catch(error){
+        console.error("Error fetching films:", error)
+
+      } 
+
+    };  
+    
+    //run function
+    fetchedFilms();
+    
+  },[]);//empty array so it runs once
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <h3>Film List</h3>
+      <ul>
+        {
+
+          //takes array stored in films and makes a list for each one, like a for loop
+          films.map((film)=>(
+            // needs unique key to track each item(useful for re-rendering)
+            <li key={film.film_id}> 
+              <strong>{film.title}</strong>-  {film.rental_count}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+
+  );
+
+
 }
 
 export default Home;
