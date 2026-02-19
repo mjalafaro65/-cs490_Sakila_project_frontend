49c35d6e114b176d3321853efb9ca1f3c0e5116a
chocolate-juice <140909474+chocolate-juice@users.noreply.github.com>
2026-02-18 19:56:41 -0500

    changes to customer tables and doing merge

--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -128,7 +128,7 @@ function Films() {
             <button className="close-button" onClick={() => setSelectedFilm(null)}>X</button>
           
             <h2 className="popUp-title">{formatName(selectedFilm.title)}</h2>
-               <p><span className="label">Film ID:</span> {selectedFilm.film_id}</p>
+            <p><span className="label">Film ID:</span> {selectedFilm.film_id}</p>
             <p><span className="label">Description:</span> {selectedFilm.description}</p>
             <p><span className="label">Release Year:</span> {selectedFilm.release_year}</p>
             <p><span className="label">Rental Duration:</span> {selectedFilm.rental_duration}</p>
@@ -139,7 +139,7 @@ function Films() {
           </div>
         </div>
       )}
-    <div className="films=button">
+    <div className="films-button">
       <button 
           disabled={page === 1}
           onClick={() => setPage(page - 1)}
